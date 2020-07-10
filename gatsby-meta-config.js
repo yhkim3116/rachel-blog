dbd8dd9e54157b49f165f7ff75c9607bb049819e
Rachel <yhkim3116@gmail.com>
2020-07-10 18:36:37 +0900

    init

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Rachel의 공간`,
+  description: `개발이나 맛집이나 일상이나 등등`,
+  author: `Rachel`,
+  introduction: `프론트엔드 개발자 Rachel의 개발얘기나 맛집얘기나 일상얘기`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
